fe3dd0dc95cec29b31ba4061cd808497f1cda4ac
Martin Andreev <martin_i_a@abv.bg>
2024-10-21 17:49:49 +0300

    Setup the functionality for displaying the home page products

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const init = async () => {
     setupStore(products);
 
     const featured = store.filter((product) => product.featured === true);
+
+    display(featured, getElement(".featured-center"));
   }
 };
 
