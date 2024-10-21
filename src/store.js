cdb6014f2806883bdbd9e499bb3e876885d85502
Martin Andreev <martin_i_a@abv.bg>
2024-10-21 17:29:55 +0300

    Setup the local storage / do so the products to be saved in the local storage

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { getStorageItem, setStorageItem } from "./utils.js";
 
-let store = [];
+let store = getStorageItem("store");
 
 const setupStore = (products) => {
   store = products.map((product) => {
@@ -11,6 +11,8 @@ const setupStore = (products) => {
     const image = img[0].thumbnails.large.url;
     return { id, featured, name, price, company, colors, image };
   });
+
+  setStorageItem("store", store);
 };
 
 const findProduct = () => {};
