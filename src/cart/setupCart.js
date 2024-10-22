418c8c655f60d46845a1dd5b1bc0cddad40fd273
Martin Andreev <martin_i_a@abv.bg>
2024-10-22 21:16:34 +0300

    Start to setup the cart
    Implement the functionality for adding new item to the cart and start to implement the functionality for adding then new item to the DOM.

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -8,8 +8,33 @@ import {
 import { openCart } from "./toggleCart.js";
 import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
+
 // set items
 
+const cartItemsCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the cart
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM
+    addToCartDOM(product);
+  } else {
+    // update values
+  }
+  // more stuff coming up
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+
+init();
